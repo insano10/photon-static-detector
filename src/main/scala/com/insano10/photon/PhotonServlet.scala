a6fd7d157cdda9bf9df63001419c06801ad1d488
insano10 <jdommett@gmail.com>
2016-06-18 14:14:17 +0100

    add examples of other rest endpoints

--- src/main/scala/com/insano10/photon/PhotonServlet.scala
+++ src/main/scala/com/insano10/photon/PhotonServlet.scala
@@ -1,6 +1,9 @@
 package com.insano10.photon
 
+import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
+import org.joda.time.{DateTimeZone, DateTime}
 import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.{Created, Ok, ActionResult}
 import org.scalatra.json.JacksonJsonSupport
 import org.slf4j.{LoggerFactory, Logger}
 
@@ -10,15 +13,39 @@ class PhotonServlet extends PhotonStaticDetectorStack with JacksonJsonSupport {
 
   protected implicit val jsonFormats: Formats = DefaultFormats
 
+  private val dateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("dd-MM-yy HH:mm")
+
+  get("/photon/time") {
+
+    logger.info("Getting the time")
+    Response(dateTimeFormatter.print(new DateTime()))
+  }
+
   post("/photon/measurements") {
 
     val measurement = parsedBody.extract[Measurement]
-    logger.info(measurement.toString)
+    logger.info("Creating " + measurement.toString)
 
-    Response("Success")
+    Created()
+  }
+
+  put("/photon/measurements") {
+
+    val measurement = parsedBody.extract[Measurement]
+    logger.info("Updating " + measurement.toString)
+
+    Ok()
+  }
+
+  delete("/photon/measurements/:id") {
+
+    logger.info("Deleting " + params("id"))
+
+    Ok()
   }
 
 }
 
 case class Measurement(val measurementType: String, val value: Integer)
+
 case class Response(val message: String)
